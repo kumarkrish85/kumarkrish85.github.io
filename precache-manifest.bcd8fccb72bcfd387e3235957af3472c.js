8c135372cc29f5923c558d710e81ae54466f6c47
Krishna <kumarkrish85@gmail.com>
2021-01-11 17:29:36 +0530

    Deployement

---
+++ precache-manifest.bcd8fccb72bcfd387e3235957af3472c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87955fcfa0bb3f7da0a9e66ab07e61b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf2d0fe3dc46e8262c3a",
+    "url": "/static/css/main.9ef56da5.chunk.css"
+  },
+  {
+    "revision": "19609c6a481fa58dbdf4",
+    "url": "/static/js/2.a1af5a5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a1af5a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2d0fe3dc46e8262c3a",
+    "url": "/static/js/main.3329d839.chunk.js"
+  },
+  {
+    "revision": "0c88c9ae059e59d0ebbf",
+    "url": "/static/js/runtime-main.37a6d1fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
